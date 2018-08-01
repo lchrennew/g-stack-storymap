c89b5afdcdc721d3bd6dbc1e15aaa2f16d4b4d8e
lichun@agilean.cn <lichun@agilean.cn>
2018-08-02 03:00:34 +0800

    delete unused Plan

--- gstack-storymap-api/src/main/java/chun/li/GStack/StoryMap/api/repositories/PlanRepository.java
+++
@@ -1,8 +0,0 @@
-package chun.li.GStack.StoryMap.api.repositories;
-
-import chun.li.GStack.StoryMap.api.domain.Plan;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-public interface PlanRepository extends Neo4jRepository<Plan, Long> {
-
-}
